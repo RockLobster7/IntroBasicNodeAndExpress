498146f0c9d59f2ff30aed84a12c4d1c94ab0a82
Glitch (rocklobster7-introbasicnodeandexpress) <none>
2019-07-07 10:47:17 +0000

    🚵😀 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -1,3 +1,8 @@
+//Use the app.get() method to serve the string Hello Express, 
+//to GET requests matching the / root path. Be sure that your 
+//code works by looking at the logs, then see the results in your browser, 
+//clicking the button ‘Show Live’ in the Glitch UI.
+
 
 var express = require('express');
 var app = express();
@@ -9,22 +14,54 @@ var app = express();
 
 
 /** 1) Meet the node console. */
+console.log ("Hello World");
 
 
 /** 2) A first working Express Server */
+/*
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
+*/
 
 
 /** 3) Serve an HTML file */
+// app responds with index.HTML
+// Node global variable __dirname to calculate the path
+var indexPath = __dirname +  "/views/index.html";
+console.log (indexPath);
+
+app.get("/", function(req, res) {
+  res.sendFile(indexPath);
+});
 
 
 /** 4) Serve static assets  */
+// we've now given the user access to assets in the public folder (css)
+var assetsFolderPath = __dirname + "/public";
+app.use(express.static(assetsFolderPath));
 
 
 /** 5) serve JSON on a specific route */
-
+//serving JSON at appURL/json
+/*
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+});
+*/
 
 /** 6) Use the .env file to configure the app */
- 
+console.log (process.env.MESSAGE_STYLE);
+
+app.get("/json", (req, res) => {
+  
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+});
+
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
